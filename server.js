329d5c1087ab170c42712176b84a92aa7940d195
Ben Haus <ben@little-haus.com>
2020-05-06 15:04:54 -0400

    Logger complete

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const userRouter = require('./users/userRouter.js');
 
 const server = express();
 
+// global middleware
+server.use(express.json());
+
+
+
+//custom middleware
+/*- `logger()`
+  - `logger` logs to the console the following information about each request: request method, request url, and a timestamp
+  - this middleware runs on every request made to the API
+  */
+
+function logger(req, res, next) {
+  const timestamp = new Date();
+  console.log(`[${timestamp}] | Method: ${req.method} | Req URL: ${req.url}`)
+  next();
+};
+server.use(logger);
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
-//custom middleware
-
-function logger(req, res, next) {}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
+const db = require('./userDb.js');
 
 router.post('/', (req, res) => {
   // do your magic!
